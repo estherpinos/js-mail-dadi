6b0c8c188b58b4e2ed8336c21ac6004a2164ff15
estherpinos <estherpinos1@gmail.com>
2023-09-12 16:25:39 +0200

    Add [esercizio 2]

--- script.js
+++ script.js
@@ -42,7 +42,27 @@ document.getElementById('ok').addEventListener('click', function(){
     document.getElementById('messaggio').innerHTML=' errore, l\'utente inserito non esiste'
   }
 
-
-  
 })
+
+// DADI
+
+document.getElementById('scopri').addEventListener('click', function(){
+  
+  let numero1 = Math.floor(Math.random() *6)+1;
+  document.getElementById('player').innerHTML='Il tuo numero è: '+numero1;
+
+  let numero2 = Math.floor(Math.random() *6)+1;
+  document.getElementById('computer').innerHTML='Il numero del computer è: '+ numero2;
+
+  if(numero1>numero2){
+    document.getElementById('vincitore').innerHTML='Hai vinto!'
+    
+  }
+
+  else{
+    document.getElementById('vincitore').innerHTML='Hai perso :c'
+  }
+})
+
+
 
